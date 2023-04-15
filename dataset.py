39258737fdc3eec1f3f5ce1c42b1e6bf5769930a
Emily Wright <e.wright0124@gmail.com>
2023-04-15 13:03:05 -0400

    Used pandas to open file and separated it by columns; put into lists

--- dataset.py
+++ dataset.py
@@ -10,4 +10,15 @@
 
 import pandas as pd # Michelle, if any of you guys need help installing module lmk
 
-songname = pd.read_csv("spotify_millsongdata.csv", sep = ",")
+songs = pd.read_csv("spotify_millsongdata.csv", sep = ",")
+## print(songs["artist"])
+## df2 = songs[["artist","text"]]
+artist_list = list(set(songs["artist"])) #no duplicates 
+song_list = list(songs["song"]) #songs are unique
+link_list = list(songs["link"]) #links are unique to the song, not sure if we want to use, but just in case
+lyrics_list = list(songs["text"]) ##includes \r and \n in the list, sep = ,
+artist_lyrics = songs[["artist","text"]] #can use to sort by artist and lyrics
+artist_title = songs[["artist","song"]] #can use to sort by artist and song, no lyrics
+artist_title_lyrics = songs[["artist","song","text"]] #can use to sort by all filterable columns
+title_lyrics = songs[["song","text"]] #can use to sort by song name and lyrics
+

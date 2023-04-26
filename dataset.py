4de0ab82d9af2d166f5c5b660cf146960bffa018
Emily Wright <e.wright0124@gmail.com>
2023-04-26 16:45:54 -0400

    actually add the second csv

--- dataset.py
+++ dataset.py
@@ -1,14 +1,16 @@
 ## link to csv -> https://www.kaggle.com/datasets/notshrirang/spotify-million-song-dataset
-
+## link to csv 2 -> https://www.kaggle.com/datasets/salvatorerastelli/spotify-and-youtube 
 
 import pandas as pd 
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+#import spotipy
+#from spotipy.oauth2 import SpotifyOAuth
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+song_info = pd.read_csv("Spotify_Youtube.csv")
 songs = pd.read_csv("spotify_millsongdata.csv", sep = ",")
+
 ## print(songs["artist"])
 ## df2 = songs[["artist","text"]]
 artist_list = list(set(songs["artist"])) #no duplicates 
@@ -18,20 +20,19 @@ lyrics_list = list(songs["text"]) ##includes \r and \n in the list, sep = ,
 artist_lyrics = songs[["artist","text"]] #can use to sort by artist and lyrics
 artist_title = songs[["artist","song"]] #can use to sort by artist and song, no lyrics
 artist_title_lyrics = songs[["artist","song","text"]] #can use to sort by all filterable columns
-title_lyrics = songs[["song","text"]] #can use to sort by song name and lyrics
-
-class song_features: 
-    """Connect to Spotify Web API to find details about matched song.
-    """
-    client_id = "04fe36c0ba5846f4856fc7796ff4eab8"
-    client_secret = "9c45909506b247088e7d296bfae4ae54"
-    redirect_uri = "http://localhost:9000"
-    scope = "user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
-                                               client_id=client_id,
-                                              client_secret=client_secret,
-                                              redirect_uri=redirect_uri))
-    
+title_lyrics = songs[["song","text"]] #can use to sort by song name and lyrics 
+#class song_features: 
+ #   """Connect to Spotify Web API to find details about matched song.
+  #  """
+   # client_id = "04fe36c0ba5846f4856fc7796ff4eab8"
+    #client_secret = "9c45909506b247088e7d296bfae4ae54"
+    #redirect_uri = "http://localhost:9000"
+    #scope = "user-library-read"
+    #sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
+     #                                          client_id=client_id,
+     #                                         client_secret=client_secret,
+      #                                        redirect_uri=redirect_uri))
+    #
     # Connect matched song to spotify API
     # Find song features
     # Print out song info and details

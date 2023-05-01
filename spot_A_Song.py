5b1c23a13104978f653a166fa9a658bdf0a85f21
Emily Wright <e.wright0124@gmail.com>
2023-05-01 13:27:25 -0400

    pandas merge and drop

--- spot_A_Song.py
+++ spot_A_Song.py
@@ -1,13 +1,33 @@
 ## link to csv -> https://www.kaggle.com/datasets/notshrirang/spotify-million-song-dataset
 ## link to csv 2 -> https://www.kaggle.com/datasets/salvatorerastelli/spotify-and-youtube 
-
 import pandas as pd 
 import re
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer 
 
 song_info = pd.read_csv("Spotify_Youtube.csv", sep = ",")
 songs = pd.read_csv("spotify_millsongdata.csv", sep = ",")
+song_info.drop("Danceability", axis = 1)
+song_info.drop("Energy", axis = 1)
+song_info.drop("Key", axis = 1)
+song_info.drop("Loudness", axis = 1)
+song_info.drop("Speechiness", axis = 1)
+song_info.drop("Acousticness", axis = 1)
+song_info.drop("Instrumentalness", axis = 1)
+song_info.drop("Liveness", axis = 1)
+song_info.drop("Valence", axis = 1)
+song_info.drop("Tempo", axis = 1)
+song_info.drop("Title", axis = 1)
+song_info.drop("Channel", axis = 1)
+song_info.drop("Likes", axis = 1)
+song_info.drop("Comments", axis = 1)
+song_info.drop("Description", axis = 1)
+song_info.drop("Licensed", axis = 1)
+song_info.drop("official_video", axis = 1)
 
+df = songs.merge(song_info, left_on = ["artist","song"], right_on= ["Artist","Track"])
+df.drop("Artist", axis = 1)
+df.drop("Track", axis = 1)
+df
 #pandas stuff
 #merge + drop columns turn each column into a list
 

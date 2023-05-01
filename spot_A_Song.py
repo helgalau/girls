5687adcea768e868b77b4a43b6c166113719412a
Helga <helgajlau@hotmail.com>
2023-05-01 13:26:33 -0400

    main func

--- spot_A_Song.py
+++ spot_A_Song.py
@@ -11,12 +11,27 @@ songs = pd.read_csv("spotify_millsongdata.csv", sep = ",")
 #pandas stuff
 #merge + drop columns turn each column into a list
 
-class song:
+class Song:
     def __init__(self):
         pass
     def regexGroup(self):
         regex = r"""\w""" # not actually done
+    def search_song_lyrics():
+    # Get input from user for song lyrics
+        search = input("What lyrics do you want to search for?: ")
     
-    
+        with open('spotify_millsongdata.csv', 'r', encoding = "utf-8") as f:
+            lines = f.readlines()
+
+        # Use a list comprehension to pull the lines that have the search the user wants
+            match = [line for line in lines if search in line]
+        
+            if match:
+                print("Matching songs:")
+                for line in match:
+                    print(line)
 def main():
-    pass
+    search_song_lyrics()            
+        
+    if __name__ == "__main__":
+        main()

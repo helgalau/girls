be4021e0c81dd8870e39edfe38b0aa15235b5e83
Michelle Nguyen <mnguy1019@gmail.com>
2023-05-01 10:44:50 -0400

    delete old code

--- dataset.py
+++ dataset.py
@@ -21,22 +21,7 @@ artist_lyrics = songs[["artist","text"]] #can use to sort by artist and lyrics
 artist_title = songs[["artist","song"]] #can use to sort by artist and song, no lyrics
 artist_title_lyrics = songs[["artist","song","text"]] #can use to sort by all filterable columns
 title_lyrics = songs[["song","text"]] #can use to sort by song name and lyrics 
-#class song_features: 
- #   """Connect to Spotify Web API to find details about matched song.
-  #  """
-   # client_id = "04fe36c0ba5846f4856fc7796ff4eab8"
-    #client_secret = "9c45909506b247088e7d296bfae4ae54"
-    #redirect_uri = "http://localhost:9000"
-    #scope = "user-library-read"
-    #sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
-     #                                          client_id=client_id,
-     #                                         client_secret=client_secret,
-      #                                        redirect_uri=redirect_uri))
-    #
-    # Connect matched song to spotify API
-    # Find song features
-    # Print out song info and details
-    # Maybe recommend other songs with similar BPM?
+
 def search_song_lyrics():
     # Get input from user for song lyrics
     search = input("What lyrics do you want to search for?: ")

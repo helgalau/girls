4d0343a971f07cc6835a6df4a8b0c4d996e179b7
Emily Wright <e.wright0124@gmail.com>
2023-05-01 12:15:09 -0400

    created class and main method

--- spot_A_Song.py
+++ spot_A_Song.py
@@ -8,3 +8,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 song_info = pd.read_csv("Spotify_Youtube.csv", sep = ",")
 songs = pd.read_csv("spotify_millsongdata.csv", sep = ",")
 
+#pandas stuff
+#merge + drop columns turn each column into a list
+
+class song:
+    def __init__(self):
+        pass
+    def regexGroup(self):
+        regex = r"""\w""" # not actually done
+    
+    
+def main():
+    pass

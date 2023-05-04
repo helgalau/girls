28810ca3c75720db26bfeabdc343b007d014d9f4
Emily Wright <e.wright0124@gmail.com>
2023-05-03 22:34:44 -0400

    pandas: Put dropped columns into a list

--- spot_A_Song.py
+++ spot_A_Song.py
@@ -8,24 +8,10 @@ def pandas():
     song_info = pd.read_csv("Spotify_Youtube.csv", sep = ",")
     songs = pd.read_csv("spotify_millsongdata.csv", sep = ",")
     songs = songs.drop("link", axis = 1)
-    song_info = song_info.drop("Danceability", axis = 1)
-    song_info = song_info.drop("Energy", axis = 1)
-    song_info = song_info.drop("Key", axis = 1)
-    song_info = song_info.drop("Loudness", axis = 1)
-    song_info = song_info.drop("Speechiness", axis = 1)
-    song_info = song_info.drop("Acousticness", axis = 1)
-    song_info = song_info.drop("Instrumentalness", axis = 1)
-    song_info = song_info.drop("Liveness", axis = 1)
-    song_info = song_info.drop("Valence", axis = 1)
-    song_info = song_info.drop("Tempo", axis = 1)
-    song_info = song_info.drop("Title", axis = 1)
-    song_info = song_info.drop("Channel", axis = 1)
-    song_info = song_info.drop("Likes", axis = 1)
-    song_info = song_info.drop("Comments", axis = 1)
-    song_info = song_info.drop("Description", axis = 1)
-    song_info = song_info.drop("Licensed", axis = 1)
-    song_info = song_info.drop("official_video", axis = 1)
-    song_info = song_info.drop("Unnamed: 0", axis = 1)
+    song_info = song_info.drop(["Danceability","Energy", "Key",
+                            "Loudness","Speechiness","Acousticness",
+                            "Instrumentalness","Liveness", "Valence", "Tempo", "Title", "Channel", "Likes", 
+                            "Comments","Description","Licensed","official_video","Unnamed: 0"], axis = 1)
 
     df = songs.merge(song_info, left_on = ["artist","song"], right_on= ["Artist","Track"])
     df = df.drop("Artist", axis = 1)

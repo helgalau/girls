a59e983d4ce48e8aa31b1f2fc15411eae37e05cf
Helga <helgajlau@hotmail.com>
2023-05-08 14:30:29 -0400

    artist match

--- spot_A_Song.py
+++ spot_A_Song.py
@@ -2,6 +2,7 @@
 ## link to csv 2 -> https://www.kaggle.com/datasets/salvatorerastelli/spotify-and-youtube 
 import pandas as pd 
 import re
+#from sklearn.feature_extraction.text import TfidfVectorizer 
 
 def pandas():
     song_info = pd.read_csv("Spotify_Youtube.csv", sep = ",")
@@ -21,35 +22,31 @@ def pandas():
 
     with open('filename2.txt', 'w') as f:
         for items in x:
-            
             f.write("%s\n" % items)
 
     with open('filename2.txt', 'r') as g:
         for y in g:
-            #print(y)
-            pass
-
+            print(y)
+            
 def regexGroup():
-    with open('filename2.txt', 'r', encoding = "utf-8") as f:
-        lines = f.readlines()
-        text = ''.join(lines)
-        regex = r"""(?xm)^\['(?P<artist>.+?)',
-                    \s'(?P<name>.+?)',
-                    \s\"(?P<lyrics>.+?)\",
-                    \s'(?P<spotifyLink>.+?)',
-                    \s'(?P<albumName>.+?)',
-                    \s'(?P<albumType>.+?)',
-                    \s'(?P<track>.+?)',
-                    \s(?P<duration>.+?),
+        with open('filename2.txt', 'r', encoding = "utf-8") as f:
+            lines = f.readlines()
+        regex = r""""(?x)
+                    ^\['(?P<artist>.+?)',
+                    \s'(?P<name> .+?)',
+                    \s\"(?P<lyrics> .+?)\",
+                    \s'(?P<spotifyLink> .+?)',
+                    \s'(?P<albumName> .+?)',
+                    \s'(?P<albumType> .+?)',
+                    \s'(?P<track> .+?)',
+                    \s(?P<duration> .+?),
                     \s(?P<youtube>.+?)',
                     \s(?P<views>.+?),
-                    \s(?P<streams>.+?)\]
+                    \s(?P<streams>.+?)]
                 """
-
-        match = re.finditer(regex, text)
+        match = re.search(regex, lines)
         return match
-    
-
+        
 class Song:
     def __init__(self):
         """Initializes the artists, lyrics, links, and durations
@@ -75,33 +72,29 @@ class Song:
         """
         regex_matches = regexGroup()
         # Use list comprehension to pull matching lyrics
-        lyrics_match = [match.group('name') for match in regex_matches if 
-                        lyrics_search in match.group('lyrics')]
-        return lyrics_match
+        lyrics_match = [match for match in regex_matches if lyrics_search
+                        in match.group('lyrics')]
+        if lyrics_match:
+            return lyrics_match
         #Use list comprehension to pull matching artists        
-        #self.artist_match = [match.group('name') for match in regex_matches 
-                        #if artist_search in match.group('artist')]
-        #if self.artist_match:
-            #return self.artist_match 
+        artist_match = [match for match in regex_matches if artist_search
+                        in match.group('artist')]
+        if artist_match:
+            return lyrics_match, artist_match 
             
     def __str__(self):
-        """Give link and duration for the top match.
-        """
-        
-        
         return ""
-        
-
+         
+    
 def main():
     # Get input from user for song lyrics and artist name
      lyrics_search = input("What lyrics do you want to search for?: ")
      artist_search = input("What is the name of the artist?: ")
      x = Song()
-     pandas()
      x.search_song_lyrics(lyrics_search, artist_search)
      # Displays the matching songs and artists
-     print(f"The possible matching song(s) are: {x.search_song_lyrics(lyrics_search, artist_search)[0]}")          
-     #print(f" The possible artist(s) are: {x.artist_match}")   
+     print(f"The possible matching song(s) are: {x.lyrics_match}")          
+     print(f" The possible artist(s) are: {x.artist_match}")   
      
 if __name__ == "__main__":
      main()

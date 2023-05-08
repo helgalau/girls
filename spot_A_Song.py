4f1b71a017f24d429567327dca2660b4faf8155d
Michelle Nguyen <mnguy1019@gmail.com>
2023-05-08 12:44:48 -0400

    fixed regexgroup and search

--- spot_A_Song.py
+++ spot_A_Song.py
@@ -22,12 +22,12 @@ def pandas():
     with open('filename2.txt', 'w') as f:
         for items in x:
             
-            print(items)
             f.write("%s\n" % items)
 
     with open('filename2.txt', 'r') as g:
         for y in g:
-            print(y)
+            #print(y)
+            pass
 
 def regexGroup():
     with open('filename2.txt', 'r', encoding = "utf-8") as f:
@@ -46,7 +46,7 @@ def regexGroup():
                     \s(?P<streams>.+?)\]
                 """
 
-        match = re.search(regex, text)
+        match = re.finditer(regex, text)
         return match
     
 
@@ -75,15 +75,14 @@ class Song:
         """
         regex_matches = regexGroup()
         # Use list comprehension to pull matching lyrics
-        self.lyrics_match = [match for match in regex_matches if lyrics_search
-                        in match.group('lyrics')]
-        if self.lyrics_match:
-            return self.lyrics_match
+        lyrics_match = [match.group('name') for match in regex_matches if 
+                        lyrics_search in match.group('lyrics')]
+        return lyrics_match
         #Use list comprehension to pull matching artists        
-        self.artist_match = [match.group('artist') for match in regex_matches 
-                        if artist_search in match.group('artist')]
-        if self.artist_match:
-            return self.artist_match 
+        #self.artist_match = [match.group('name') for match in regex_matches 
+                        #if artist_search in match.group('artist')]
+        #if self.artist_match:
+            #return self.artist_match 
             
     def __str__(self):
         """Give link and duration for the top match.
@@ -101,8 +100,8 @@ def main():
      pandas()
      x.search_song_lyrics(lyrics_search, artist_search)
      # Displays the matching songs and artists
-     print(f"The possible matching song(s) are: {x.lyrics_match}")          
-     print(f" The possible artist(s) are: {x.artist_match}")   
+     print(f"The possible matching song(s) are: {x.search_song_lyrics(lyrics_search, artist_search)[0]}")          
+     #print(f" The possible artist(s) are: {x.artist_match}")   
      
 if __name__ == "__main__":
      main()

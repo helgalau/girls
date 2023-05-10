53d682d7920b2292571fa441e6b125de59b36367
Emily Wright <e.wright0124@gmail.com>
2023-05-10 17:16:39 -0400

    Docstrings and some formatting

--- spot_A_Song.py
+++ spot_A_Song.py
@@ -1,19 +1,22 @@
-"""I think we need a docstring for the project?"""
-"""Use filename2.txt for seraching song lyrics"""
+"""A module for finding a song match based on lyrics and artist.
+"""
 ## link to csv -> https://www.kaggle.com/datasets/notshrirang/spotify-million-song-dataset
 ## link to csv 2 -> https://www.kaggle.com/datasets/salvatorerastelli/spotify-and-youtube 
 
 import matplotlib.pyplot as plt
 import pandas as pd 
 import re
-#from sklearn.feature_extraction.text import TfidfVectorizer 
 
 def mergefiles():
     """Opens up the two csv files containing the songs' information and 
-    combines them. Then the combined data is turned into a list that is stored in a txt file.
+    combines them. Then the combined data is turned into a list that is stored
+    in a txt file.
+    
+    Returns:
+        df(Pandas Dataframe): New dataframe that includes the songs information
     
     Side effects:
-    Creates filename2 and populates it with a dataframe information
+        Creates filename2.txt and populates it with a dataframe information
     """
     song_info = pd.read_csv("Spotify_Youtube.csv", sep = ",")
     songs = pd.read_csv("spotify_millsongdata.csv", sep = ",")
@@ -21,10 +24,12 @@ def mergefiles():
     song_info = song_info.drop(["Energy", "Key",
                             "Loudness","Speechiness","Acousticness",
                             "Instrumentalness","Liveness", "Valence", "Tempo",
-                            "Title", "Channel", "Likes", "Comments","Description",
-                            "Licensed","official_video","Unnamed: 0"], axis = 1)
+                            "Title", "Channel", "Likes", "Comments",
+                            "Description","Licensed","official_video",
+                            "Unnamed: 0"], axis = 1)
 
-    df = songs.merge(song_info, left_on = ["artist","song"], right_on= ["Artist","Track"])
+    df = songs.merge(song_info, left_on = ["artist","song"],
+                     right_on= ["Artist","Track"])
     df = df.drop("Artist", axis = 1)
     df = df.drop("Track", axis = 1)
     x =df.to_numpy().tolist()
@@ -60,6 +65,8 @@ def regexGroup():
     return match
         
 class Song:
+    """
+    """
     def __init__(self):
         """Initializes the artists, lyrheics, links, and durations
            of the songs in an empty list.
@@ -93,19 +100,11 @@ class Song:
                             lyrics_search in match.group('lyrics')]
         else:
             lyrics_match = [match.group('name') for match in reg_match if
-                            lyrics_search in match.group('lyrics') and artist_search in match.group('artist')]
+                            lyrics_search in match.group('lyrics') and
+                            artist_search in match.group('artist')]
             
         self.lyrics.append(lyrics_match)
-        self.artists.append(artist_search)
-       
-        #if artist_search != "not given": 
-          # artist_match = [match.group('name') for match in lyrics_match if
-            #              artist_search in match.group('artist')]
-        
-           # return lyrics_match, artist_match
-        # Use list comprehension to pull matching lyrics
-        #return lyrics_match
-        #Use list comprehension to pull matching artists        
+        self.artists.append(artist_search)      
 
         return (lyrics_match[0], artist_search)
     
@@ -126,7 +125,8 @@ class Song:
 
     
     def data_vis(self, song):
-        #Data Visualization: Bar Graph for user's inputted song and danceability score
+        #Data Visualization: Bar Graph for user's inputted song and
+        #danceability score
         df = self.datafr
         
         df_song = df[df["song"] == song]
@@ -138,8 +138,10 @@ class Song:
         plt.title(f"Danceability for {song}")
         plot_results = plt.show()
         
-        #Data Visualization #2: Sort the danceability score in descending order to get the top 5 danceability scores
-        top_5_dance_scores = df.sort_values('Danceability', ascending = False).head(5)
+        #Data Visualization #2: Sort the danceability score in descending order to get
+        #the top 5 danceability scores
+        top_5_dance_scores = df.sort_values('Danceability',
+                                            ascending = False).head(5)
 
         plt.bar(top_5_dance_scores['song'], top_5_dance_scores['Danceability'])
         plt.xticks(rotation=90)
@@ -190,7 +192,8 @@ def main():
     availabilityCheck = False
     artistCheck = False
     lyrics_search = input("What lyrics do you want to search for?: ")
-    #Iterates until user inputs a valid input (lyric and artist) for the check_availability method
+    #Iterates until user inputs a valid input (lyric and artist) for the 
+    #check_availability method
     while(availabilityCheck is False):
         if (Song.check_availability(lyrics_search) is True):
             artist_search = input("What is the name of the artist? (Put \"not given\" if unknown ): ")
@@ -210,7 +213,8 @@ def main():
     # Displays the matching songs and artists
     print(f"The possible matching song(s) are: {x}")   
     
-    #Displayes the second bar graph of the top 5 danceability scores and songs -> gives users ability to compare their song to the top 5 songs to see if it has a high danceability score       
+    #Displayes the second bar graph of the top 5 danceability scores and songs ->
+    # gives users ability to compare their song to the top 5 songs to see if it has a high danceability score       
     danceability_search = input("Want to check to see if you can dance to this song? What is your song name? ")
     x.data_vis(danceability_search)
      
